bec54e77c88a2bc2e7da3fee1da48e9149207013
Daniel Andreas Wang <danielaw@getacademy.no>
2026-01-06 14:54:21 +0100

    add types

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+}
+
+export interface CartItem extends Product {
+    quantity: number;
+}
+
+export interface AppState {
+    products: Product[];
+    cart: CartItem[];
+    currentView: 'products' | 'cart';
+}
