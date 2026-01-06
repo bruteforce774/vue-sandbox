de7ea927a0475ac28dbbdedd3c4fe0f0a23caa56
Daniel Andreas Wang <danielaw@getacademy.no>
2026-01-06 17:19:49 +0100

    borrow placeholder store

---
+++ src/stores/cartStore.ts
@@ -0,0 +1,7 @@
+import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+
+export const useCounterStore = defineStore('counter', () => {
+  // state, actions, getters here
+  return { /* expose everything */ }
+})
