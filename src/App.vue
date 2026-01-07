2b5fd681eb936400993c71f9a48748997c772825
Daniel Andreas Wang <danielaw@getacademy.no>
2026-01-07 17:29:24 +0100

    show product list

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Header from './components/Header.vue'
+import ProductList from './components/ProductList.vue';
 </script>
 
 <template>
@@ -8,4 +9,12 @@ import Header from './components/Header.vue'
     :current-view="'products'" 
     @view-changed="(view) => console.log('view changed:', view)" 
   />
+  <ProductList 
+    :products="[
+      { id: 1, name: 'Product A', price: 100, image: 'https://via.placeholder.com/150' },
+      { id: 2, name: 'Product B', price: 200, image: 'https://via.placeholder.com/150' },
+      { id: 3, name: 'Product C', price: 300, image: 'https://via.placeholder.com/150' }
+    ]" 
+    @add-to-cart="(productId) => console.log('Add to cart:', productId)"
+    />
 </template>
