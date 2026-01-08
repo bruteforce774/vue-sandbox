f970b1be73fa6312d7808d1080684302cfd998be
Daniel Andreas Wang <danielaw@getacademy.no>
2026-01-08 23:54:41 +0100

    code cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import Header from './components/Header.vue'
-import ProductList from './components/ProductList.vue';
-import ShopView from './views/ShopView.vue';
 import './style.css'
 </script>
 
