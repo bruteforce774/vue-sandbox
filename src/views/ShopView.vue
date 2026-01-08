f5b4b8e74bbc0bff496a24386567d608f6ecdab6
Daniel Andreas Wang <danielaw@getacademy.no>
2026-01-08 16:30:35 +0100

    start on ShopView

---
+++ src/views/ShopView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    import { onMounted } from 'vue';
+    import { useCartStore } from '../stores/cartStore';
+    const cartStore = useCartStore();
+    onMounted(() => {
+        cartStore.fetchProducts();
+    });
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
