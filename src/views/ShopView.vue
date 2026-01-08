d43e067b1ca65e9534a7c9f699a4ded1315e616e
Daniel Andreas Wang <danielaw@getacademy.no>
2026-01-08 22:39:01 +0100

    wip

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
-    import { onMounted } from 'vue';
+    import { ref, onMounted } from 'vue';
     import { useCartStore } from '../stores/cartStore';
     import Header from '../components/Header.vue';
     import ProductList from '../components/ProductList.vue';
+    import Cart from '../components/Cart.vue';
+    
     const cartStore = useCartStore();
+    const currentView = ref('products');
+
     onMounted(() => {
         cartStore.fetchProducts();
     });
 </script>
 
 <template>
-    <Header :cart-count="cartStore.cartCount" :current-view="'products'" @view-changed="(view) => console.log('view changed:', view)" />
-    <ProductList :products="cartStore.products" @add-to-cart="(productId) => console.log('Add to cart:', productId)" />
+    <div>
+        <Header :cart-count="cartStore.cartCount" :current-view="currentView" @view-changed="(view) => currentView = view" />
+    </div>
+    <div v-if="currentView === 'products'">
+        <ProductList :products="cartStore.products" @add-to-cart="cartStore.addToCart" />
+    </div>
+    <div v-else>
+        <Cart :items="cartStore.cartItems" :total="cartStore.cartTotal" />
+    </div>
 </template>
 
 <style scoped>
