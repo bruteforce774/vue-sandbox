631f75b0e2449cf63c12a499b3799b27b8f0d143
Daniel Andreas Wang <danielaw@getacademy.no>
2026-01-08 23:18:29 +0100

    set up route structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,25 @@
-import { createRouter, createWebHistory } from "vue-router"
-import ShopView from "../views/ShopView.vue"
+import { createRouter, createWebHistory } from "vue-router";
+import ShopView from "../views/ShopView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-        path: "/",
-        name: "shop",
-        component: ShopView
-    }
-]
-})
+      path: "/",
+      name: "products",
+      component: ShopView,
+    },
+    {
+      path: "/cart",
+      name: "cart",
+      component: ShopView,
+    },
+    // {
+    //     path: "/product/:id",
+    //     name: "product-detail",
+    //     component: ProductDetail
+    // }
+  ],
+});
 
-export default router
+export default router;
