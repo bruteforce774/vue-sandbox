47d1e8ae1a67bde258cbd6cfad6d6ac7f1b27acc
Daniel Andreas Wang <danielaw@getacademy.no>
2026-01-08 21:15:29 +0100

    display ShopView

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
 import Header from './components/Header.vue'
 import ProductList from './components/ProductList.vue';
+import ShopView from './views/ShopView.vue';
+import './style.css'
 </script>
 
 <template>
-  <Header 
-    :cart-count="3" 
-    :current-view="'products'" 
-    @view-changed="(view) => console.log('view changed:', view)" 
-  />
-  <ProductList 
-    :products="[
-      { id: 1, name: 'Product A', price: 100, image: 'https://via.placeholder.com/150' },
-      { id: 2, name: 'Product B', price: 200, image: 'https://via.placeholder.com/150' },
-      { id: 3, name: 'Product C', price: 300, image: 'https://via.placeholder.com/150' }
-    ]" 
-    @add-to-cart="(productId) => console.log('Add to cart:', productId)"
-    />
+  <div id="app">
+    <ShopView />
+  </div>
 </template>
--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
     import { onMounted } from 'vue';
     import { useCartStore } from '../stores/cartStore';
+    import Header from '../components/Header.vue';
+    import ProductList from '../components/ProductList.vue';
     const cartStore = useCartStore();
     onMounted(() => {
         cartStore.fetchProducts();
@@ -8,6 +10,8 @@
 </script>
 
 <template>
+    <Header :cart-count="cartStore.cartCount" :current-view="'products'" />
+    <ProductList :products="cartStore.products"/>
 </template>
 
 <style scoped>
